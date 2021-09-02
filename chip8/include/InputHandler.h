dccdb7f367688151fc8458787273bd03f7db5c31
mipmip5241 <michaelg5241@gmail.com>
2021-09-02 21:39:41 +0300

    Added FX33

---
+++ chip8/include/InputHandler.h
@@ -0,0 +1,16 @@
+#pragma once
+#include <unordered_map>
+#include <SFML/Graphics.hpp>
+
+class InputHandler
+{
+public:
+	InputHandler();
+	~InputHandler()=default;
+	void update_keys_states();
+
+	const std::unordered_map<sf::Keyboard::Key, bool> get_key_states() const;
+private:
+	std::unordered_map<sf::Keyboard::Key, bool> _keys_state;
+};
+
---
+++ chip8/src/Game.cpp
@@ -0,0 +1,14 @@
+#include "../include/Core.h"
+#include <iostream>
+
+
+int main()
+{
+	std::srand(time(NULL));
+	Core chip;
+	chip.set_opcode(0x0123);
+	chip.emulate_cycle();
+	return 0;
+}
+
+
